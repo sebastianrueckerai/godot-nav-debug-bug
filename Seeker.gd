c3691d8acdec5cf870b533fab95ec0cc71445977
unknown <Ruecker.Sebastian@googlemail.com>
2022-08-22 23:56:30 +0200

    initial

---
+++ Seeker.gd
@@ -0,0 +1,19 @@
+extends KinematicBody
+
+export var speed : float = 5.0
+
+var velocity: Vector3 = Vector3.ZERO
+var direction : Vector3 = Vector3.ZERO
+
+onready var navigation_agent = $NavigationAgent
+onready var target = $"%Target"
+
+func _ready():
+	navigation_agent.set_target_location(target.global_transform.origin)
+
+func _physics_process(delta):
+	var point = navigation_agent.get_next_location()
+	direction = global_transform.origin.direction_to(point)
+	
+	velocity = velocity.linear_interpolate(direction * speed, speed * delta)
+	velocity = move_and_slide(velocity, Vector3.UP)
